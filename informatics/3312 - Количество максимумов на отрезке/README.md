daceb80d8e775f9daaf347dba194005609b5a835
Mark <mark.vkm@gmail.com>
2017-07-31 16:08:11 +0600

    informatics, 3312 - Количество максимумов на отрезке

---
+++ informatics/3312 - Количество максимумов на отрезке/README.md
@@ -0,0 +1,7 @@
+### Topic
+
+    Segment Tree
+
+### Problem Source
+
+[informatics](http://informatics.mccme.ru/mod/statements/view3.php?chapterid=3312#1)
---
+++ informatics/3312 - Количество максимумов на отрезке/solution.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstring>
+
+#define F first
+#define S second
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define pii pair<int, int>
+
+const int inf = 1e9;
+const int N = 1e5+5;
+const int mod = 1e9+7;
+
+using namespace std;
+
+int n, a[N], k, l, r;
+pii t[4*N];
+
+void build(int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v].F = a[tl];
+        t[v].S = 1;
+        return;
+    }
+
+    int tm = (tl+tr)/2;
+    build(2*v, tl, tm);
+    build(2*v+1, tm+1, tr);
+
+    if (t[2*v].F == t[2*v+1].F) {
+        t[v] = t[2*v];
+        t[v].S += t[2*v+1].S;
+    }
+    else t[v] = max(t[2*v], t[2*v+1]);
+}
+
+pii getMax(int v, int tl, int tr, int l, int r) {
+    if (tr < l || r < tl)
+        return pii(-inf, 0);
+
+    if (l <= tl && tr <= r)
+        return t[v];
+
+    int tm = (tl+tr)/2;
+
+    pii a = getMax(v+v,tl,tm,l,min(r,tm)),
+            b = getMax(2*v+1, tm+1, tr, max(tm+1,l), r);
+    if(a.F==b.F)
+        return mp(a.F, a.S+b.S);
+    else
+        return max(a,b);
+}
+
+int main() {
+    speed
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    build(1, 1, n);
+
+    cin >> k;
+    for (int i = 1; i <= k; i++) {
+        cin >> l >> r;
+        pii ans = getMax(1, 1, n, l, r);
+        cout << ans.F << ' ' << ans.S << endl;
+    }
+    return 0;
+}
