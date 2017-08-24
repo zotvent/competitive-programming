91c09aeb20ad5d7e6f1aca8dcff43fda3b52c460
Mark <mark.vkm@gmail.com>
2017-08-24 11:02:58 +0600

    informatics, 1120 - 0-1 рюкзак точный вес

---
+++ informatics/1120 - 0-1 рюкзак точный вес/README.md
@@ -0,0 +1,7 @@
+### Topic
+
+    Dynamic Programming, Knapsack problem
+
+### Problem Source
+
+[informatics](http://informatics.mccme.ru/mod/statements/view3.php?id=813&chapterid=1120#1)
---
+++ informatics/1120 - 0-1 рюкзак точный вес/solution.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstring>
+
+#define F first
+#define S second
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define pii pair<int, int>
+
+const int inf = 1e9;
+const int N = 10005;
+const int mod = 16714589;
+const int MAX = 2500;
+const int shift = 4;
+
+using namespace std;
+
+int n, x[105], s, dp[N];
+
+void input() {
+    cin >> n >> s;
+    for (int i = 1; i <= n; i++)
+        cin >> x[i];
+}
+
+void solve() {
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = s-x[i]; j >= 0; j--)
+            if (dp[j])
+                dp[j+x[i]] = 1;
+}
+
+void output() {
+    cout << (dp[s] ? "YES" : "NO");
+}
+
+int main() {
+    input();
+    solve();
+    output();
+    return 0;
+}
