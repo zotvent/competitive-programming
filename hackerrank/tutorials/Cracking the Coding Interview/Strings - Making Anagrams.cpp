f5b2e240237e373d641a60974c2afc2be15b1da8
Mark <mark.vkm@gmail.com>
2017-11-14 23:55:39 +0600

    hackerrank, tutorials - Cracking the Coding Interview

---
+++ hackerrank/tutorials/Cracking the Coding Interview/Strings - Making Anagrams.cpp
@@ -0,0 +1,45 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int number_needed(string a, string b) {
+    vector<int> cnt(26, 0);
+    for (auto c: a)
+        cnt[c-'a']++;
+    for (auto c: b)
+        cnt[c-'a']--;
+    int res = 0;
+    for (auto value: cnt)
+        res += abs(value);
+    return res;
+}
+
+int main(){
+    string a;
+    cin >> a;
+    string b;
+    cin >> b;
+    cout << number_needed(a, b) << endl;
+    return 0;
+}
