2b01bb9e75fcfcedf4377539e02ce07ea1b2c398
Mark <mark.vkm@gmail.com>
2018-01-22 22:39:10 +0600

    leetcode - algorithms

---
+++ leetcode/algorithms/112 - Path Sum.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (!root) return false;
+        if (root->val == sum && !root->left && !root->right) return true;
+        else return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+    }
+};
