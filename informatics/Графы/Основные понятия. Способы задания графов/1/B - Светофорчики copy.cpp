4bca1bceea518dbbae262dde98c9464d08a6f685
Mark <mark.vkm@gmail.com>
2018-09-18 23:29:11 +0600

    informatics, Основные понятия. Способы задания графов - 1

---
+++ informatics/Графы/Основные понятия. Способы задания графов/1/B - Светофорчики copy.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define mp make_pair
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int inf = 1e9;
+const int N = 1e2+5;
+const int mod = 1743;
+const int MAX = 1e6;
+const int shift = 4;
+
+class Solution {
+    
+    int i, j;
+    int n, m, from, to, cnt[N];
+
+    void print() {
+    }
+    
+    void input() {
+        speed
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        cin >> n >> m;
+        for (i = 0; i < m; i++) {
+            cin >> from >> to;
+            cnt[from]++;
+            cnt[to]++;
+        }
+    }
+    
+    void output() {
+        for (i = 1; i <= n; i++) {
+            cout << cnt[i] << ' ';
+        }
+    }
+    
+    void solution() {
+        
+    }
+    
+public:
+    void solve() {
+        input();
+        solution();
+        output();
+    }
+};
+
+int main() {
+    Solution s = Solution();
+    s.solve();
+    return 0;
+}
