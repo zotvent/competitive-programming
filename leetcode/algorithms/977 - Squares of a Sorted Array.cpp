67b45450369fa68f819bc23f0740c15f6708b6b6
Mark Kim <zotvent@gmail.com>
2019-01-20 21:50:45 +0600

    leetcode, 977 - Squares of a Sorted Array

---
+++ leetcode/algorithms/977 - Squares of a Sorted Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int> res;
+        
+        for (int i = 0; i < A.size(); i++)
+            res.push_back(A[i] * A[i]);
+        sort(res.begin(), res.end());
+        
+        return res;
+    }
+};
