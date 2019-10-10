f4b5cf99920d5cdb643676872c17bca33ff337dc
Mark Kim <zotvent@gmail.com>
2019-10-10 23:06:36 +0600

    informatics, Поиск и сортировка - Быстрая сортировка

--- informatics/Поиск и сортировка/Быстрая сортировка/766 - Сортировка слиянием.cpp
+++ informatics/Поиск и сортировка/Быстрая сортировка/766 - Сортировка слиянием.cpp
@@ -27,7 +27,7 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int inf = 1e9 + 5;
+const int inf = 1e9;
 const int N = 1e5 + 5;
 const int mod = 1743;
 const int MAX = 1e6;
@@ -35,8 +35,7 @@ const int shift = 4;
 
 class Solution {
 
-    int n;
-    vector<int> a;
+    int n, a[N];
 
     void print() {
     }
@@ -46,9 +45,8 @@ class Solution {
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
         cin >> n;
-        a.resize(n);
         for (int i = 0; i < n; i++) {
-            cin >> a[i];
+          cin >> a[i];
         }
     }
 
@@ -56,52 +54,42 @@ class Solution {
     }
 
     void output() {
-        for (int i = 0; i < a.size(); i++) {
-            cout << a[i] << ' ';
+        for (int i = 0; i < n; i++) {
+          cout << a[i] << ' ';
         }
     }
 
     void solution() {
-        quick_sort(a, 0, n);
+      quick_sort(0, n - 1);
     }
 
-    void quick_sort(vector<int> &array, int l, int r) {
-        if (l < r) {
-            int q = random_partition(array, l, r);
-            if (q == -1) {
-                return;
-            }
-            quick_sort(array, l, q);
-            quick_sort(array, q + 1, r);
-        }
+    void quick_sort(int start, int end) {
+      if (start < end) {
+        int m = random_partition(start, end);
+        quick_sort(start, m - 1);
+        quick_sort(m + 1, end);
+      }
     }
 
-    int random_partition(vector<int> &array, int l, int r) {
-        int i = rand() % (r - l);
-        swap(array[l + i], array[r - 1]);
-        return partition(array, l, r);
+    int random_partition(int start, int end) {
+      int pivot = start + rand() % (end - start + 1);
+      swap(a[end], a[pivot]);
+      return partition(start, end);
     }
 
-    int partition(vector<int> &array, int l, int r) {
-        int x = array[r - 1];
-        int i = l - 1;
-        int cnt = 0;
+    int partition(int start, int end) {
+      int pivot = end;
+      int index = start;
 
-        for (int j = l; j < r - 1; j++) {
-            if (array[j] <= x) {
-                cnt += (array[j] == x ? 1 : 0);
-                swap(array[++i], array[j]);
-            }
+      for (int i = start; i < end; i++) {
+        if (a[i] < a[pivot]) {
+          swap(a[i], a[index]);
+          index++;
         }
+      }
+      swap(a[pivot], a[index]);
 
-        swap(array[++i], array[r - 1]);
-
-        // case if all numbers are equal
-        if (cnt == r - l - 1) {
-            i = -1;
-        }
-
-        return i;
+      return index;
     }
 
 public:
@@ -110,11 +98,11 @@ public:
     }
 
     void solve() {
+        prepare();
         input();
         solution();
         output();
     }
-
 };
 
 int main() {
