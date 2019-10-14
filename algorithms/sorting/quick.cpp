be2290c6b6f8e3dda032071631de4d940b8ae94d
Mark Kim <zotvent@gmail.com>
2019-10-14 19:04:58 +0600

    algorithms, sorting - quick sort

--- algorithms/sorting/quick.cpp
+++ algorithms/sorting/quick.cpp
@@ -1,44 +1,33 @@
 // usage example
 // n - size of the array
-int n;
-vector<int> a;
-quick_sort(a, 0, n);
+int n, int a[n];
+quick_sort(0, n - 1);
 
-void quick_sort(vector<int> &array, int l, int r) {
-    if (l < r) {
-        int q = random_partition(array, l, r);
-        if (q == -1) {
-            return;
-        }
-        quick_sort(array, l, q);
-        quick_sort(array, q + 1, r);
-    }
+void quick_sort(int start, int end) {
+  if (start < end) {
+    int m = random_partition(start, end);
+    quick_sort(start, m - 1);
+    quick_sort(m + 1, end);
+  }
 }
 
-int random_partition(vector<int> &array, int l, int r) {
-    int i = rand() % (r - l);
-    swap(array[l + i], array[r - 1]);
-    return partition(array, l, r);
+int random_partition(int start, int end) {
+  int pivot = start + rand() % (end - start + 1);
+  swap(a[end], a[pivot]);
+  return partition(start, end);
 }
 
-int partition(vector<int> &array, int l, int r) {
-    int x = array[r - 1];
-    int i = l - 1;
-    int cnt = 0;
+int partition(int start, int end) {
+  int pivot = end;
+  int index = start;
 
-    for (int j = l; j < r - 1; j++) {
-        if (array[j] <= x) {
-            cnt += (array[j] == x ? 1 : 0);
-            swap(array[++i], array[j]);
-        }
+  for (int i = start; i < end; i++) {
+    if (a[i] < a[pivot]) {
+      swap(a[i], a[index]);
+      index++;
     }
+  }
+  swap(a[pivot], a[index]);
 
-    swap(array[++i], array[r - 1]);
-
-    // case if all numbers are equal
-    if (cnt == r - l - 1) {
-        i = -1;
-    }
-
-    return i;
+  return index;
 }
