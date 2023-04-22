242db5b3a4ecbb52b70105b24f87158d5ebe105a
Mark Kim <mark.vkm@gmail.com>
2023-04-22 16:28:40 +0100

    algorithms, leetcode

--- leetcode/algorithms/15 - 3Sum.cpp
+++ leetcode/algorithms/15 - 3Sum.cpp
@@ -2,36 +2,27 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
+        const int n = nums.size();
         
         sort(nums.begin(), nums.end());
-        
-        int l, r, sum;
-        int target = 0;
-        
-        for (int i = 0; i < (int) nums.size() - 2; i++) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
-                target = -nums[i];
-                l = i + 1;
-                r = nums.size() - 1;
-                
-                while (l < r) {
-                    sum = nums[l] + nums[r];
-                    
-                    if (sum < target) l++;
-                    else if (sum > target) r--;
-                    else {
-                        res.push_back({nums[i], nums[l], nums[r]});
-                        
-                        while (l < r && nums[l] == nums[l + 1]) l++;
-                        while (l < r && nums[r] == nums[r - 1]) r--;
-                        
-                        l++;
-                        r--;
-                    }
+        for (int i = 0; i < n - 2; i++) {
+            int target = -nums[i];
+            int l = i + 1, r = n - 1;
+            
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum == target) {
+                    res.push_back({nums[i], nums[l++], nums[r--]});
+                    while (l < r && nums[l] == nums[l - 1]) l++;
+                    while (l < r && nums[r] == nums[r + 1]) r--;
                 }
+                else if (sum > target) r--;
+                else l++;
             }
+
+            while (i < n - 2 && nums[i] == nums[i + 1]) i++;
         }
-        
+
         return res;
     }
 };
