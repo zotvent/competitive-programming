0e0c19eb74e39885787e00c2f9f59bf3d029668b
Mark Kim <mark.vkm@gmail.com>
2023-04-23 22:14:16 +0100

    Update 278 - First Bad Version.cpp

--- leetcode/algorithms/278 - First Bad Version.cpp
+++ leetcode/algorithms/278 - First Bad Version.cpp
@@ -4,21 +4,12 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int l = 1;
-        int r = n;
-        int m;
-        
+        int l = 1, r = n;
         while (l < r) {
-            m = l + (r - l) / 2;
-            
-            if (isBadVersion(m)) {
-                r = m;
-            }
-            else {
-                l = m + 1;
-            }
+            int m = l + (r - l) / 2;
+            if (isBadVersion(m)) r = m;
+            else l = m + 1;
         }
-        
         return l;
     }
 };
