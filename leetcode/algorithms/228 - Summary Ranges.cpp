f37b84f2020a3d03457990663403a8fb578518e9
Mark Kim <mark.vkm@gmail.com>
2023-04-23 21:15:05 +0100

    algorithms, leetcode

--- leetcode/algorithms/228 - Summary Ranges.cpp
+++ leetcode/algorithms/228 - Summary Ranges.cpp
@@ -1,40 +1,21 @@
 class Solution {
-    
-    string range(int start, int end) {
-        string res = to_string(start);
-        
-        if (end > start) {
-            res += "->" + to_string(end);
-        }
-        
-        return res;
+
+    string range(int s, int e) {
+        if (s == e) return to_string(s);
+        return to_string(s) + "->" + to_string(e);
     }
-    
+
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> res;
-        
-        if (nums.empty()) {
-            return res;
-        }
-        
-        const int n = nums.size();
-        int start, end;
-        start = end = nums[0];
-        
-        for (int i = 1; i <= n; i++) {
-            if (i == n) {
-                res.push_back(range(start, end));
-            }
-            else if (nums[i] > nums[i - 1] + 1) {
-                res.push_back(range(start, end));
-                start = end = nums[i];
-            }
-            else {
-                end = nums[i];
+        int start = nums.empty() ? -1 : 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1] + 1) {
+                res.push_back(range(nums[start], nums[i - 1]));
+                start = i;
             }
         }
-        
+        if (start != -1) res.push_back(range(nums[start], nums.back()));
         return res;
     }
 };
