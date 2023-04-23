ae3ca17d1ee92b8c3c2abd15a5e96c0577e81d94
Mark Kim <mark.vkm@gmail.com>
2023-04-23 22:25:06 +0100

    Update 69 - Sqrt(x).cpp

--- leetcode/algorithms/69 - Sqrt(x).cpp
+++ leetcode/algorithms/69 - Sqrt(x).cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int res;
-        
-        int l = 0;
-        int r = x;
-        
-        while (l <= r) {
-            long long m = (r - l) / 2 + l;
-            long long square = m * m;
-            
-            if (square == x) {
-                res = m;
-                break;
-            }
-            else if (square > x) {
-                r = m - 1;
-            }
-            else {
-                res = m;
-                l = m + 1;
-            }
+        long long l = 1, r = x;
+        while (l < r) {
+            long long m = l + (r - l) / 2;
+            if (m * m > x) r = m;
+            else l = m + 1;
         }
-        
-        return res;
+        return l * l <= x ? l : l - 1;
     }
 };
