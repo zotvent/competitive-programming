6857bcc83f0a28e4dc349f4be0e3745607111198
Mark Kim <mark.vkm@gmail.com>
2023-04-23 21:45:21 +0100

    Update 79 - Word Search.cpp

--- leetcode/algorithms/79 - Word Search.cpp
+++ leetcode/algorithms/79 - Word Search.cpp
@@ -1,48 +1,41 @@
 class Solution {
-private:
-    const int dx[4] = {0, 1, 0, -1};
-    const int dy[4] = {1, 0, -1, 0};
-    
-    int rows;
-    int cols;
-    vector<vector<char>> board;
-    
-public:
-    bool exist(vector<vector<char>>& board, string word) {
-        rows = board.size();
-        cols = (board.empty() ? 0 : board[0].size());
-        this->board = board;
-                
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[i].size(); j++) {
-                if (dfs(i, j, word, 0)) {
+
+    const vector<vector<int>> d = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    bool valid(int row, int col, int rows, int cols) {
+        return row >= 0 && col >= 0 && row < rows && col < cols;
+    }
+
+    bool backtrack(vector<vector<char>>& board, int row, int col, string& word, int head) {
+        if (head == word.size() - 1) return true;
+        
+        const int n = board.size();
+        const int m = board[0].size();
+        char letter = board[row][col];
+        board[row][col] = '.';
+        for (auto& i: d) {
+            int r = row + i[0];
+            int c = col + i[1];
+            if (valid(r, c, n, m) && board[r][c] == word[head + 1]) {
+                if (backtrack(board, r, c, word, head + 1)) {
+                    board[row][col] = letter;
                     return true;
                 }
             }
         }
-        
+        board[row][col] = letter;
         return false;
     }
-    
-    bool dfs(int r, int c, string word, int head) {
-        if (head >= word.size()) {
-            return true;
-        }
-        
-        if (r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] != word[head]) {
-            return false;
-        }
-        
-        board[r][c] = '#';
-        
-        for (int i = 0; i < 4; i++) {
-            if (dfs(r + dx[i], c + dy[i], word, head + 1)) {
-                return true;
+
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[i].size(); j++) {
+                if (board[i][j] == word[0] && backtrack(board, i, j, word, 0)) {
+                    return true;
+                }
             }
         }
-        
-        board[r][c] = word[head];
-        
         return false;
     }
 };
