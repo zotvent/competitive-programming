72584ac9cd49535cc518edb00a83b45ced5a4701
Mark Kim <mark.vkm@gmail.com>
2023-05-14 18:33:14 +0100

    Update 557 - Reverse Words in a String III.cpp

--- leetcode/algorithms/557 - Reverse Words in a String III.cpp
+++ leetcode/algorithms/557 - Reverse Words in a String III.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int head = 0;
-        int n = s.size();
-        
-        for (int i = 0; i < n; i++) {
-            if (s[i] == ' ' || i == n - 1) {
-                reverse(s.begin() + head, s.begin() + i + (i == n - 1));
-                head = i + 1;
-            }
+        const int n = s.size();
+        for (int i = 0; i < n;) {
+            if (s[i] != ' ') {
+                int start = i;
+                while (i < n && s[i] != ' ') i++;
+                reverse(s.begin() + start, s.begin() + i);
+            } else i++;
         }
-        
         return s;
     }
 };
